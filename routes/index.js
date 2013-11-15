1bf74edc7bd6806348ba5f80b6a00c7cae122be1
Keivaun <keivaun@keivaun-baus.(none)>
2013-11-14 23:57:58 -0600

    Limit subject length

--- routes/index.js
+++ routes/index.js
@@ -12,34 +12,17 @@ exports.users = require('./users');
 exports.user = require('./user');
 exports.manage = require('./manage');
 
+var databaseUrl = 'blog';
+var collections = ['posts'];
+var db = require('mongojs').connect(databaseUrl, collections);
 
 exports.index = function(req, res){
-  var databaseUrl = 'blog';
-  var collections = ['posts'];
-  var db = require('mongojs').connect(databaseUrl, collections);
   var subject = req.param('subject');
   var content = req.param('content');
-  var d = new Date();
-  
   
   console.log("subject: ", subject, "   content: ", content);
   
-  if(req.param('subject') && req.param('content')){
-    db.posts.save({'name': req.session.name, 'username': req.session.username, 'subject': subject, 'content': content, 'date': d}, function(err, savedPost){
-      if(err)
-        console.log('ERROR');
-      db.posts.findOne({'username': req.session.username, 'subject': subject, 'content': content}, function(err, doc){
-        if(err)
-          console.log('Error');
-        console.log('Document id is ' + doc._id);
-        res.redirect('/' + doc._id);
-      });
-    });
-  }
-  else{
-    db.posts.find().limit(10).sort({date:-1}, function(error, docs){
-      res.render('index', {'name': 'Everyone', 'docs': docs});
-    });
-  }
-  
+  db.posts.find().limit(10).sort({date:-1}, function(error, docs){
+    res.render('index', {'name': 'Everyone', 'docs': docs});
+  });
 };
--- routes/newpost.js
+++ routes/newpost.js
@@ -1,13 +1,37 @@
+var databaseUrl = 'blog';
+var collections = ['posts'];
+var db = require('mongojs').connect(databaseUrl, collections);
+var d = new Date();
+
 module.exports = function(req,res){
+  var subject = req.param('subject');
+  var content = req.param('content');
+  
   if(req.session.isLoggedIn){
     if(!req.param('subject') && !req.param('content')){
-      console.log('Subject: ' + req.param('subject') + 'Content: ' + req.param('content'));
+      console.log('Subject: ' + subject + 'Content: ' + content);
       res.render('newpost'); 
     }
     else{
+      if(req.param('subject') && req.param('content')){
+        if(req.param('subject').length > 40){
+          res.render('newpost', {error: 'Please limit your subject to 40 characters'});
+          return;
+        }
+        db.posts.save({'name': req.session.name, 'username': req.session.username, 'subject': subject, 'content': content, 'date': d}, function(err, savedPost){
+          if(err)
+            console.log('ERROR');
+          db.posts.findOne({'username': req.session.username, 'subject': subject, 'content': content}, function(err, doc){
+            if(err)
+              console.log('Error');
+            console.log('Document id is ' + doc._id);
+            res.redirect('/' + doc._id);
+          });
+        });
+      }
       console.log('in else');
       res.render('newpost', {error: 'Please provide both a subject and content'});
-      console.log('Subject: ' + req.param('subject') + ' Content: ' + req.param('content'));
+      console.log('Subject: ' + subject + ' Content: ' + content);
     }
   }
   else{
