cd6a54df4e80338b405f1d65333b61c330937ded
Keivaun Waugh <keivaunwaugh@gmail.com>
2016-12-26 22:06:34 -0500

    removed unnecessary logs.

--- routes/morePosts.js
+++ routes/morePosts.js
@@ -7,9 +7,6 @@ module.exports = function(req, res){
     }
     POSTS.find().sort({date: -1}, function(err, docs){
         var arr = docs.splice(req.session.morePosts, 10);
-        console.log('req.session.morePosts:', req.session.morePosts);
-        console.log('arr:', arr);
-        console.log('arr.length:', arr.length);
         if(arr.length === 0){
             res.json({error: 'There are no more posts'});
         }
