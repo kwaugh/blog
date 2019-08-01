d4eb2aa8f11853446d9f53a4cca4dd9be9797c41
Keivaun Waugh <keivaun@keivaun.com>
2019-08-01 18:18:54 +0100

    switch to sha256 of master password.

--- routes/password.js
+++ routes/password.js
@@ -1,11 +1,15 @@
 // handles the master password for the site
+
+var crypto = require('crypto');
+
 module.exports = function(req, res) {
     if (req.session.master_password) {
         res.redirect('/');
         return;
     }
     if(req.param('password')){
-        DB.collection('master').findOne({'password': req.param('password')},
+        var hash = crypto.createHash('sha256').update(req.param('password')).digest('hex')
+        DB.collection('master').findOne({'password': hash},
             function(err, doc) {
                 if (doc) {
                     req.session.master_password = true;
